6d527d59099e99c27a395c8d97b51245984a2a2f
Aryansingh0810 <ss0854850@gmail.com>
2026-03-26 15:41:37 +0530

    Initial commit

---
+++ OneBox/OneBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneBoxApp.swift
+//  OneBox
+//
+//  Created by Aryan singh on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct OneBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
